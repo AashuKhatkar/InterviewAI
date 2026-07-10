38b5eda598b5d27deb0076d60dd23515fb2a8e8c
Aashu Khatkar <Aashukhatkar2006@gmail.com>
2026-07-10 12:32:35 +0530

    Added popup after logout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { ServerUrl } from '../App'
 import { setUserData } from '../redux/userSlice'
+import AuthModel from './AuthModel'
 
 function Navbar() {
     const {userData} = useSelector((state) => state.user)
@@ -15,6 +16,7 @@ function Navbar() {
     const [showUserPopup,setShowUserPopup] = useState(false)
     const navigate = useNavigate()
     const dispatch = useDispatch()
+    const [showAuth, setShowAuth] = useState(false);
 
     const handleLogout = async() => {
         try {
@@ -45,7 +47,12 @@ function Navbar() {
 
                 <div className = 'flex items-center gap-6 relative'>
                     <div className = 'relative'>
-                        <button onClick={()=>{setShowCreditPopup(!showCreditPopup);
+                        <button onClick={()=>{
+                            if(!userData) {
+                                setShowAuth(true)
+                                return;
+                            }
+                            setShowCreditPopup(!showCreditPopup);
                             setShowUserPopup(false)
                         }}
                         className = 'flex items-center gap-2 bg-gray-100 px-4 py-2 rounded-full text-md hover:bg-gray-200 transition'>
@@ -62,7 +69,12 @@ function Navbar() {
                     </div>
 
                     <div className = 'relative'>
-                        <button onClick={()=>{setShowUserPopup(!showUserPopup);
+                        <button onClick={()=>{
+                            if(!userData) {
+                                setShowAuth(true)
+                                return;
+                            }
+                            setShowUserPopup(!showUserPopup);
                             setShowCreditPopup(false)
                         }}
                         className = 'w-9 h-9 bg-black text-white rounded-full flex items-center justify-center font-semibold'>
@@ -70,7 +82,7 @@ function Navbar() {
                         </button>
 
                         {showUserPopup && (
-                            <div className = 'absolute right-0 mt-3 w-48 bg-white shadow-xl border-gray-200 rounded-xl p-4 z-50'>
+                            <div className = 'absolute right-0 mt-3 w-48 bg-white shadow-xl border border-gray-200 rounded-xl p-4 z-50'>
                                 <p className='text-md text-blue-500 font-medium mb-1'>{userData?.name}</p>
 
                                 <button onClick={()=>navigate("/history")} 
@@ -83,6 +95,7 @@ function Navbar() {
                     </div>
                 </div>
             </motion.div>
+            {showAuth && <AuthModel onClose={()=> setShowAuth(false)}/>}
         </div>
     )
 }
